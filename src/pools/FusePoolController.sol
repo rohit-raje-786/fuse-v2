8d0adaf62328f19de5baa873362eca374b6c35c0
JetDeveloping <jetjadeja@gmail.com>
2021-12-08 09:26:06 -0800

    Implement auth on FusePoolController (for tests)

--- src/pools/FusePoolController.sol
+++ src/pools/FusePoolController.sol
@@ -1,13 +1,15 @@
 pragma solidity 0.8.10;
 
+import {Auth, Authority} from "lib/solmate/src/Auth/Auth.sol";
+
 /// @title Fuse Pool Controller
 /// @author Jet Jadeja <jet@rari.capital>
 /// @notice This contract is used to manage the Fuse Pool. TODO: change this
-contract FusePoolController {
+contract FusePoolController is Auth {
     string public name;
     string public symbol;
 
-    constructor(string memory _name, string memory _symbol) {
+    constructor(string memory _name, string memory _symbol) Auth(msg.sender, Authority(msg.sender)) {
         name = _name;
         symbol = _symbol;
     }
