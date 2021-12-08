b1b9e7490714402f1c58e320d956fa4ffb4a6789
JetDeveloping <jetjadeja@gmail.com>
2021-12-07 18:30:38 -0800

    Remove contract interfaces

--- src/pools/interfaces/IFusePoolController.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.10;
-
-/// @notice Fuse Pool Controller Interface
-/// @author Jet Jadeja <jet@rari.capital>
-interface IFusePoolController {
-    
-}
