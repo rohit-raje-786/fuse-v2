fd1e9459180d5dd5f9bfd89a656fd5590b385274
JetDeveloping <jetjadeja@gmail.com>
2021-12-08 07:10:58 -0800

    Add pool controller constructor for testing

--- src/pools/FusePoolController.sol
+++ src/pools/FusePoolController.sol
@@ -6,4 +6,9 @@ pragma solidity 0.8.10;
 contract FusePoolController {
     string public name;
     string public symbol;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
 }
