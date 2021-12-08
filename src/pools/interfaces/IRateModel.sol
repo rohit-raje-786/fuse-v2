c7f2f1a778db9e59133d9be4d5c4df230bf218dc
JetDeveloping <jetjadeja@gmail.com>
2021-12-07 18:34:13 -0800

    Implement IRateModel interface

---
+++ src/pools/interfaces/IRateModel.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.10;
+
+/// @title Interest Rate Model Interface
+/// @author Jet Jadeja <jet@rari.capital>
+interface IRateModel {
+    /// @notice Calculate the current borrow interest rate per block.
+    function getBorrowRate(uint256, uint256, uint256) external view returns (uint256);
+
+    /// @notice Calculate the current supply interest per block. 
+    function getSupplyRate(uint256, uint256, uint256) external view returns (uint256);
+}
