b8669e9fbc566eadf7509edda00fdaf473474157
JetDeveloping <jetjadeja@gmail.com>
2021-12-09 09:37:27 -0800

    Add deployFuseToken method to controller for testing

--- src/pools/FusePoolController.sol
+++ src/pools/FusePoolController.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
 
+import {FuseToken, IRateModel} from "./FuseToken.sol";
+
+import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
 import {Auth, Authority} from "lib/solmate/src/Auth/Auth.sol";
 
 /// @title Fuse Pool Controller
@@ -13,5 +16,19 @@ contract FusePoolController is Auth {
     constructor(string memory _name, string memory _symbol) Auth(msg.sender, Authority(msg.sender)) {
         name = _name;
         symbol = _symbol;
+    }
+
+    function deployFuseToken(
+        ERC20 token,
+        uint256 lendFactor,
+        uint256 borrowFactor,
+        IRateModel rateModel,
+        uint256 reserveRate,
+        uint256 feeRate
+    ) external returns (FuseToken) {
+        FuseToken fuseToken = new FuseToken(token, this);
+        fuseToken.initialize(lendFactor, borrowFactor, rateModel, reserveRate, feeRate);
+
+        return fuseToken;
     }
 }
