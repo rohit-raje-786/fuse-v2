9e489a113af07a2b5458aa6573f04f2dc8ad771f
JetDeveloping <jetjadeja@gmail.com>
2021-12-19 00:40:57 -0800

    Add price oracle interface

---
+++ src/pools/interfaces/IPriceOracle.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: AGPL-3.0-only
+
+import {FusePoolToken} from "../FusePoolToken.sol";
+
+/// @title Price Oracle Interface
+/// @author Jet Jadeja <jet@rari.capital>
+/// @notice Oracle that enables us to retrieve the price of any asset.
+interface IPriceOracle {
+    /// @notice Get the price of an asset.
+    /// @param fToken The fToken to get the underlying price of.
+    /// @dev The underlying asset price is scaled by 1e18. 
+    /// A value of zero means the price is not available.
+    function getUnderlyingPrice(FusePoolToken fToken) external view returns (uint256);
+}
