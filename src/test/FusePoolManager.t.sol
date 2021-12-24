dd412b49fabec87254dd310365ceb13b25f17887
JetDeveloping <jetjadeja@gmail.com>
2021-12-23 18:23:39 -0800

    Implement FusePoolToken deployment test

--- src/test/FusePoolManager.t.sol
+++ src/test/FusePoolManager.t.sol
@@ -14,11 +14,30 @@ contract FusePoolManagerTest is DSTestPlus {
     MockERC20 underlying;
     TrustAuthority authority;
 
-    FusePoolManager poolManager;
+    FusePoolManager manager;
 
     function setUp() public {
         // Deploy Fuse contracts
         underlying = new MockERC20("Mock Token", "MT", 18);
         authority = new TrustAuthority(address(this));
-        poolManager = new FusePoolManager(Authority(address(authority)), "Fuse Pool Manager", "FPN");
+        manager = new FusePoolManager(Authority(address(authority)), "Fuse Pool Manager", "FPN");
     }
+
+    function testFusePoolTokenDeployment() public {
+        // Deploy Fuse Pool Token.
+        FusePoolToken token = manager.deployFusePoolToken(underlying, 1e17, 1e18, IRateModel(address(0)), 1e18, 1e18);
+
+        // Retrieve values.
+        (uint256 lendFactor, uint256 borrowFactor) = manager.assets(token);
+        IRateModel rateModel = token.rateModel();
+        uint256 reserveRate = token.reserveRate();
+        uint256 feeRate = token.feeRate();
+
+        // Ensure that token variables are valid.
+        assertEq(lendFactor, 1e17);
+        assertEq(borrowFactor, 1e18);
+        assertEq(address(rateModel), address(0));
+        assertEq(reserveRate, 1e18);
+        assertEq(feeRate, 1e18);
+    }
+}
