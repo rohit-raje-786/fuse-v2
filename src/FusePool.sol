123243c04b41a6642133a5ecb96c2ae0828a38e5
JetDeveloping <jetjadeja@gmail.com>
2022-01-14 20:28:19 -0800

    Implement FusePool constructor

--- src/FusePool.sol
+++ src/FusePool.sol
@@ -1,2 +1,26 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
+
+import {Auth, Authority} from "solmate/auth/Auth.sol";
+
+import {FusePoolFactory} from "./FusePoolFactory.sol";
+
+/// @title Fuse Pool
+/// @author Jet Jadeja <jet@rari.capital>
+/// @notice Minimal, gas optimized lending market
+contract FusePool is Auth {
+    /*///////////////////////////////////////////////////////////////
+                                IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The name of the Fuse Pool
+    string public name;
+
+    /// @notice Creates a new FusePool.
+    /// @dev Retrieves the pool name from the state of the FusePoolFactory.
+    constructor() Auth(Auth(msg.sender).owner(), Auth(msg.sender).authority()) {
+        // Retrieve the name from the FusePoolFactory
+        // and set it as the name of the FusePool.
+        name = FusePoolFactory(msg.sender).poolDeploymentName();
+    }
+}
