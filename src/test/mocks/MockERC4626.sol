a54a297f3f6b2c90f383592031993fa9c0152681
JetDeveloping <jetjadeja@gmail.com>
2022-01-17 18:05:25 -0800

    Implement mock 4626 vault

---
+++ src/test/mocks/MockERC4626.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.10;
+
+import {ERC20, ERC4626} from "solmate-next/mixins/ERC4626.sol";
+
+contract MockERC4626 is ERC4626 {
+    constructor(
+        ERC20 underlying,
+        string memory name,
+        string memory symbol
+    ) public ERC4626(underlying, name, symbol) {}
+
+    function beforeWithdraw(uint256 underlyingAmount) internal override {}
+
+    function afterDeposit(uint256 underlyingAmount) internal override {}
+
+    function totalHoldings() public view override returns (uint256) {
+        return underlying.balanceOf(address(this));
+    }
+}
