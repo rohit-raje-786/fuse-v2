20bb5f83d45debf1c045d474af2da3b2313616e9
JetDeveloping <jetjadeja@gmail.com>
2022-01-17 17:55:08 -0800

    Implement FusePoolFactory test contract

--- src/test/FusePoolFactory.t.sol
+++ src/test/FusePoolFactory.t.sol
@@ -1,2 +1,34 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
+
+import {FusePool, FusePoolFactory} from "../FusePoolFactory.sol";
+import {Authority} from "solmate-next/auth/Auth.sol";
+import {DSTestPlus} from "solmate-next/test/utils/DSTestPlus.sol";
+
+/// @title Fuse Pool Factory Test Contract
+contract FusePoolFactoryTest is DSTestPlus {
+    // Used variables.
+    FusePoolFactory factory;
+
+    function setUp() public {
+        // Deploy Fuse Pool Factory.
+        factory = new FusePoolFactory(address(this), Authority(address(0)));
+    }
+
+    function testDeployFusePool() public {
+        (FusePool pool, uint256 id) = factory.deployFusePool("Test Pool");
+
+        // Assertions.
+        assertEq(address(pool), address(factory.getPoolFromNumber(id)));
+        assertGt(address(pool).code.length, 0);
+    }
+
+    function testPoolNumberIncrement() public {
+        (FusePool pool1, uint256 id1) = factory.deployFusePool("Test Pool 1");
+        (FusePool pool2, uint256 id2) = factory.deployFusePool("Test Pool 2");
+
+        // Assertions.
+        assertFalse(id1 == id2);
+        assertFalse(pool1 == pool2);
+    }
+}
