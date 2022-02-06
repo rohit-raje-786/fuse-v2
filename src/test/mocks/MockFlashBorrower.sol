689193b7671a94159f67c31ba3491254d0cbd853
JetDeveloping <jetjadeja@gmail.com>
2022-02-05 19:11:55 -0800

    Fix flash lend test

--- src/test/mocks/MockFlashBorrower.sol
+++ src/test/mocks/MockFlashBorrower.sol
@@ -1,14 +1,21 @@
-import {FusePool, ERC20} from "../../FusePool.sol";
+import {FusePool, ERC4626, ERC20} from "../../FusePool.sol";
+import "ds-test/test.sol";
 
 /// @title Mock Flash Borrower
 /// @dev A test implementation of the FlashBorrower contract.
-contract MockFlashBorrower {
+contract MockFlashBorrower is DSTest {
     /// @dev Called by the FusePool contract after a flash loan.
     function execute(uint256 amount, bytes memory data) external {
         // Retrieve the asset from the data.
-        address asset = abi.decode(data, (address));
+        ERC20 asset = ERC20(abi.decode(data, (address)));
 
-        // Deposit tokens back into the FusePool contract.
-        FusePool(msg.sender).vaults(ERC20(asset)).deposit(asset, amount);
+        // Get the address of the Vault.
+        ERC4626 vault = FusePool(msg.sender).vaults(asset);
+
+        // Approve tokens to the Vault.
+        asset.approve(address(vault), amount);
+
+        // Deposit tokens back into the Vault contract on behalf of the Fuse Pool.
+        vault.deposit(msg.sender, amount);
     }
 }
