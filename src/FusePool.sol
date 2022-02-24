3f7e0cb8306d88a4c10045e978ad07455239b477
JetDeveloping <jetjadeja@gmail.com>
2022-02-24 11:10:00 -0700

    Implement constructor + config for immutables

--- src/FusePool.sol
+++ src/FusePool.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.10;
 
+import {FusePoolFactory} from "./FusePoolFactory.sol";
+
 import {ERC20} from "solmate-next/tokens/ERC20.sol";
+import {Auth, Authority} from "solmate-next/auth/Auth.sol";
 
 import {SafeTransferLib} from "solmate-next/utils/SafeTransferLib.sol";
 import {SafeCastLib} from "solmate-next/utils/SafeCastLib.sol";
@@ -10,6 +13,22 @@ import {FixedPointMathLib} from "solmate-next/utils/FixedPointMathLib.sol";
 /// @title Fuse Pool
 /// @author Jet Jadeja <jet@rari.capital>
 /// @notice Minimal, gas optimized lending market
-contract FusePool {
+contract FusePool is Auth {
+    using SafeTransferLib for ERC20;
+    using SafeCastLib for uint256;
+    using FixedPointMathLib for uint256;
 
+    /*///////////////////////////////////////////////////////////////
+                                IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Pool name.
+    string public name;
+
+    /// @notice Create a new Fuse Pool.
+    /// @dev Retrieves the pool name from the FusePoolFactory contract.
+    constructor() Auth(Auth(msg.sender).owner(), Auth(msg.sender).authority()) {
+        // Retrieve the name from the factory contract.
+        name = FusePoolFactory(msg.sender).poolDeploymentName();
+    }
 }
