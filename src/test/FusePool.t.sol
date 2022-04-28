2a4b9958d29535a95a130e40c8605c6d3893d8f1
JetJadeja <jetjadeja@gmail.com>
2022-04-28 11:23:55 -0700

    Use Solmate bound implementation

--- src/test/FusePool.t.sol
+++ src/test/FusePool.t.sol
@@ -344,28 +344,6 @@ contract FusePoolTest is DSTestPlus {
         underlying.mint(address(this), amount);
         underlying.approve(address(pool), amount);
     }
-
-    // Bound a value between a min and max.
-    function bound(
-        uint256 x,
-        uint256 min,
-        uint256 max
-    ) internal pure returns (uint256 result) {
-        require(max >= min, "MAX_LESS_THAN_MIN");
-
-        uint256 size = max - min;
-
-        if (max != type(uint256).max) size++; // Make the max inclusive.
-        if (size == 0) return min; // Using max would be equivalent as well.
-        // Ensure max is inclusive in cases where x != 0 and max is at uint max.
-        if (max == type(uint256).max && x != 0) x--; // Accounted for later.
-
-        if (x < min) x += size * (((min - x) / size) + 1);
-        result = min + ((x - min) % size);
-
-        // Account for decrementing x to make max inclusive.
-        if (max == type(uint256).max && x != 0) result++;
-    }
 }
 
 interface HEVM {
